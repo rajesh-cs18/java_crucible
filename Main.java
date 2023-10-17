c83d7497872aa14c45d74ca46ad21cba4e8ae121
Rajesh Kumar <60205922+rajesh-cs18@users.noreply.github.com>
2023-10-18 01:28:41 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class SyntaxErrorExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int x = 10;
+        y = 20; // missing semicolon
+        System.out.println(x + y);
+    }
+}
